06efdbcb326351345cac88005dfd4721fa6fbd04
Kiru <kelvin.kipruto@student.moringaschool.com>
2023-04-03 00:09:25 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let list = document.querySelectorAll('#list');
+  console.log(list);
+
+  let input = document.querySelector('#new-task-description')
+  let taskForm = document.querySelector('#create-task-form');
+
+  console.log(taskForm);
+  taskForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      let list = document.querySelector('#tasks')
+      let listContent = document.createElement('li')
+      listContent.innerText = input.value
+      // console.log(listContent)
+      
+      let handleDelete = document.createElement('button')
+      handleDelete.innerText = 'x'
+
+      list.appendChild(listContent);
+      listContent.appendChild(handleDelete);
+      taskForm.reset()
+
+
+      // delete/remove task
+      handleDelete.addEventListener('click', (e) => {
+          listContent.remove();
+
+      })
+
+
+      //resets input form
+      taskForm.reset()
+  })
+
+
+
 });
